3ca9b4bbb2e0d27f2c37abb8dcb4ae9848740fa8
IBartosz Zdybel <zdybelbartosz@gmail.com>
2022-04-26 14:19:05 +0200

    Added JFrame

---
+++ src/Display.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends Canvas implements Runnable{
+
+    private Thread thread;
+    private JFrame frame;
+    private static String title = "3D model";
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static boolean running = false;
+
+    public Display(){
+        this.frame = new JFrame();
+        Dimension size = new Dimension(WIDTH,HEIGHT);
+        this.setPreferredSize(size);
+    }
+
+    public static void main(String[] args){
+        Display display = new Display();
+        display.frame.setTitle(title);
+        display.frame.add(display);
+        display.frame.pack();
+        display.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        display.frame.setLocationRelativeTo(null);
+        display.frame.setResizable(false);
+        display.frame.setVisible(true);
+
+        display.start();
+    }
+
+    public synchronized void start(){
+        running = true;
+        this.thread = new Thread(this, "Display");
+        this.thread.start();
+    }
+
+    public synchronized void stop(){
+        running = false;
+        try{
+            this.thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+
+    }
+}
